cd8ad50beb6e77f95789573013fe08cd3e6274ef
Or <orishlach20@gmail.com>
2025-12-15 09:42:58 +0200

    feat(client-routes): add empty routing for profile settings and feature pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router'
 import Home from './pages/HomePage/HomePage';
 import styles from './styles/App.module.css';
+import Profile from './pages/ProfilePage/ProfilePage';
+import Settings from './pages/SettingsPage/SettingsPage';
+import Feature from './pages/FeaturePage/FeaturePage';
 
 import projectLogo from './assets/project-logo.png'
 
@@ -17,6 +20,9 @@ function App() {
         <main className={styles.main}>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/feature" element={<Feature />} />
           </Routes>
         </main>
         <footer className={styles.footer}>
